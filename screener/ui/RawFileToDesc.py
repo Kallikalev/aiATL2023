b4852b2c0936bfb82ccbb5f552d5a8dd18a5f841
boon <boon@sahra.attlocal.net>
2023-11-19 01:57:26 -0500

    fixed syntax errors in langchain

--- screener/ui/RawFileToDesc.py
+++ screener/ui/RawFileToDesc.py
@@ -4,7 +4,7 @@ from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from git_output_sample_data.json_nico import test_dict
 
-def rawToDesc(skills, rawFile, repoDesc, readMe): #skills string, rawFile string, repoDesc string
+def rawToDesc(skills, rawFile, repoName, readMe): #skills string, rawFile string, repoDesc string
     template = """The following is a list of skills provided from this candidates resume.
     An optional repo description from this users Github is provided.
     The optional ReadMe is provided as well. Do not fill in any unprovided details.
@@ -13,7 +13,7 @@ def rawToDesc(skills, rawFile, repoDesc, readMe): #skills string, rawFile string
 
     Raw File: {rawFile}
 
-    Repo Description: {repoDesc}
+    Repo Name: {repoName}
 
     ReadMe: {readMe}
     
@@ -32,12 +32,12 @@ def rawToDesc(skills, rawFile, repoDesc, readMe): #skills string, rawFile string
     """
 
     llm = VertexAI(model_name="code-bison",max_output_tokens=1000,temperature=0.3)
-    prompt = PromptTemplate(template=template, input_variables=[skills,rawFile,repoDesc,readMe])
+    prompt = PromptTemplate(template=template, input_variables=["skills","rawFile","repoName","readMe"])
 
 
     llm_chain = LLMChain(prompt=prompt,llm=llm)
 
-    response = llm_chain.run({"skills":skills,"rawFile":rawFile,"repoDesc":repoDesc,"readMe":readMe})
+    response = llm_chain.run({"skills":skills,"rawFile":rawFile,"repoName":repoName,"readMe":readMe})
     return response
 
 if __name__ == "__main__":
